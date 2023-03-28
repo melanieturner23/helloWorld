54a5ac6f4f6ab5a2706d14c33127a25bc4180dc4
Melanie Turner <mturne14@terpmail.umd.edu>
2023-03-28 11:40:39 -0400

    Changes Made: - Added new route called /favorite-course - Enabled new favorite-course route to accept query parameters and return sentence stating favorite course entered - Added new route called /contact - Created contact.html template to take in first name, last name, email, and a button confirming they agreed to be contacted

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,15 @@ def hello():  # put application's code here
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
+@app.route('/favorite-course')
+def favorite_course():
 
+    return render_template('favorite-course.html')
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 if __name__ == '__main__':
     app.run()
